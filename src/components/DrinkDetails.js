9cf333f753ad9fb90042b333a5a063d6bee9e0ef
melissa-morgan <melissamorganc@gmail.com>
2022-01-09 23:16:02 -0500

    pulling drink data on drink options

--- src/components/DrinkDetails.js
+++ src/components/DrinkDetails.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import BrowseDrinks from './BrowseDrinks';
+import DrinkOptions from './DrinkOptions';
 
 function DrinkDetails({drinks}) {
 
--- src/components/DrinkOptions.js
+++ src/components/DrinkOptions.js
@@ -13,20 +13,39 @@ function DrinkOptions(props) {
         fetch(url)
         .then((res) => res.json())
         .then((json) => {
-            setDrinks(json)
+            setDrinks(json.drinks)
             console.log(json)
         })
         .catch(console.error)
     }, []);
 
-    return (
-        <section className='drinksContainer'>
-            <Link to='/drinks/vodka'>Vodka</Link>
-            <Link to='/drinks/rum'>Rum</Link>
-            <Link to='/drinks/whiskey'>Whiskey</Link>
+    if (!drinks) {
+			return <p>Loading yummy drinks...</p>;
+    }
 
-            {/* I dont think this should be drinks and maybe needs to be drink types instead? and then map it to drinks on the next layer? */}
-            {/* {drinks.map(drink => {
+    return (
+			<section className='drinksContainer'>
+				{drinks.map((drink) => (
+					<Link to='/drinks/gin' key={drink.idDrink}>
+						<div className='card'>
+							<div className='cardImage'>
+								<img src={drink.strDrinkThumb} alt={drink.strDrink} />
+							</div>
+							<div className='cardTitle'>
+								<h3>{drink.strDrink}</h3>
+							</div>
+						</div>
+						Gin
+					</Link>
+				))}
+				<Link to='/drinks/rum'>Rum</Link>
+				<Link to='/drinks/tequila'>Tequila</Link>
+				<Link to='/drinks/vodka'>Vodka</Link>
+				<Link to='/drinks/whiskey'>Whiskey</Link>
+
+
+				{/* I dont think this should be drinks and maybe needs to be drink types instead? and then map it to drinks on the next layer? */}
+				{/* {drinks.map(drink => {
                 return (
                     <Link to={`browsedrinks/${drink.strIngredient1}`} key={drink.strIngredient1}>
                         <div className='card'>
@@ -40,7 +59,7 @@ function DrinkOptions(props) {
                     </Link>
                 )
             })} */}
-            {/* // <select name='slectList' id='selectList' className='alcoholOptions'>
+				{/* // <select name='slectList' id='selectList' className='alcoholOptions'>
             //     <option selected value="Browse Drinks"></option>
             //     <option value="option 1">Vodka</option>
             //     <option value="option 2">Tequila</option>
@@ -48,7 +67,7 @@ function DrinkOptions(props) {
             //     <option value="option 4">Rum</option>
             //     <option value="option 5">Non-Alcoholic</option>
             // </select> */}
-        // </section>
+			</section>
 		);
 }
 
