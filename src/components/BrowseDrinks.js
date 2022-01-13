85f1ed3c1c4d62f85ef8a7736539c2e36b6a6f4c
melissa-morgan <melissamorganc@gmail.com>
2022-01-13 10:09:50 -0500

    code review clean up

--- src/components/BrowseDrinks.js
+++ src/components/BrowseDrinks.js
@@ -1,26 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { Link } from 'react-router-dom';
-import DrinkDetails from './DrinkDetails';
+import React from 'react';
 import DrinkOptions from './DrinkOptions';
 
 
 function BrowseDrinks(props) {
-    
-    const {strIngredient1} = useParams();
-
-    useEffect(() => {
-        getDrinkData();
-    }, []);
-
-    function getDrinkData() {
-        const url = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${strIngredient1}`
-
-    }
 
     return (
 			<div>
-				<DrinkOptions />
+                <DrinkOptions />
 			</div>
 		);
 }
